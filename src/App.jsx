348367c009eaeadc5982b3e6b57e2feeb34239ed
Shravani-Nigade <160117942+Shravani-Nigade@users.noreply.github.com>
2026-04-02 15:13:50 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Paradise Nursery</h1>
+      <p>Welcome to Paradise Nursery - Your one-stop shop for beautiful plants!</p>
+      
+      <button>Get Started</button>
+    </div>
+  );
+}
+
+export default App;
